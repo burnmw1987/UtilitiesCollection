aeb4fd2ec43c7e9e0f42e291036c26672d194611
burnmw1987 <burnmw1987>
2018-01-30 19:52:51 +0000

    Initial project commit

---
+++ UtilitiesCollection/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace UtilitiesCollection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(780, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Analyze";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(487, 472);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(505, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(350, 472);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(681, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(699, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 528);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Utilities Collection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ UtilitiesCollection/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+
+
+namespace UtilitiesCollection
+{
+    public partial class Form1 : Form
+    {
+        private String str_TextBoxBody;
+        private String str_FileName;
+        private FileSystemWatcher watcher;
+        //@"C:\Development\DevSOA\config\results.xml"
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Create a new FileSystemWatcher and set its properties.
+            watcher = new FileSystemWatcher();
+
+            /* Watch for changes in LastAccess and LastWrite times, and 
+               the renaming of files or directories. */
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            // Only watch text files.
+            //watcher.Path = @"C:\Development\DevSOA\config\";
+            //watcher.Filter = "results.xml";
+
+            // Add event handlers.
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+
+            // Begin watching.
+            //watcher.EnableRaisingEvents = true;
+
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            PerformOperation();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PerformOperation();            
+        }
+
+        private void PerformOperation()
+        {
+
+            if (InvokeRequired)
+            {
+                MethodInvoker method = new MethodInvoker(PerformOperation);
+                Invoke(method);
+                return;
+            }
+
+            //str_TextBoxBody = "";
+            listBox1.Items.Clear();
+
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.Load(str_FileName);
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            listBox1.BeginUpdate();
+            foreach (XmlNode node in doc.SelectNodes("//testcase"))
+            {
+                str_TextBoxBody = "";
+                foreach (XmlAttribute attribute in node.Attributes)
+                {
+                    str_TextBoxBody += attribute.Value + "; ";
+                }
+                listBox1.Items.Add(str_TextBoxBody);
+                
+            }
+            listBox1.EndUpdate();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            string selectedItemText = listBox1.SelectedItem.ToString();
+            if (selectedItemText.Contains("Failure"))
+            {
+                string testName = selectedItemText.Split(';')[0];
+
+                XElement test = XElement.Load(str_FileName);
+                var query = from param in test.Descendants("testcase")
+                            where ((string)param.Attribute("name")).Contains(testName)
+                            select new
+                            {
+                                failureShort   = (string)param.Element("failure").Attribute("message"),
+                                failureVerbose = (string)param.Element("failure")
+                                
+                            };
+                foreach (var item in query)
+                {
+                    textBox1.Text = item.failureShort + Environment.NewLine + Environment.NewLine + item.failureVerbose;
+                }
+            }
+            else
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            //
+            // Draw the background of the ListBox control for each item.
+            // Create a new Brush and initialize to a Black colored brush
+            // by default.
+            //
+            e.DrawBackground();
+            Brush myBrush = Brushes.Black;
+            //
+            // Determine the color of the brush to draw each item based on 
+            // the index of the item to draw.
+            //
+            if (listBox1.Items[e.Index].ToString().Contains("Failure"))
+            {
+                myBrush = Brushes.Red;
+            }
+            //
+            // Draw the current item text based on the current 
+            // Font and the custom brush settings.
+            //
+            e.Graphics.DrawString(((ListBox)sender).Items[e.Index].ToString(),
+                e.Font, myBrush, e.Bounds, StringFormat.GenericDefault);
+            //
+            // If the ListBox has focus, draw a focus rectangle 
+            // around the selected item.
+            //
+            e.DrawFocusRectangle();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.Filter = "XML Files (*.xml)|*.xml|Text Files (.txt)|*.txt";
+            openFileDialog1.FilterIndex = 1;
+
+            DialogResult dialogResult1 = openFileDialog1.ShowDialog();
+            if (dialogResult1 == DialogResult.OK)
+            {
+                str_FileName = openFileDialog1.FileName;
+                textBox2.Text = str_FileName;
+                watcher.Path = Path.GetDirectoryName(str_FileName);
+                watcher.Filter = Path.GetFileName(str_FileName);
+                watcher.EnableRaisingEvents = true;
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            str_FileName = textBox2.Text;
+            watcher.Path = Path.GetDirectoryName(str_FileName);
+            watcher.Filter = Path.GetFileName(str_FileName);
+            watcher.EnableRaisingEvents = true;
+        }
+    }
+}
